9a231cac9be354f881af67006a6929a79516175a
Mikael Zhang <mz@asseco.dk>
2021-08-26 16:01:47 +0200

    Add FORBIDDEN to prevent other controller actions from being invoked

--- src/main/java/org/dcsa/uisupport/controller/EventDeliveryController.java
+++ src/main/java/org/dcsa/uisupport/controller/EventDeliveryController.java
@@ -7,8 +7,10 @@ import org.dcsa.uisupport.service.EventDeliveryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
@@ -24,5 +26,26 @@ public class EventDeliveryController extends ExtendedBaseController<EventDeliver
         return eventDeliveryService;
     }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public Mono<EventDelivery> create(@Valid @RequestBody EventDelivery eventDelivery) {
+        return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN));
+    }
+
+    @PutMapping(path = "{eventID}")
+    public Mono<EventDelivery> update(@PathVariable UUID eventID, @Valid @RequestBody EventDelivery eventDelivery) {
+        return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN));
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public Mono<Void> delete(@RequestBody EventDelivery eventDelivery) {
+        return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN));
+    }
+
+    @DeleteMapping(path = "{eventID}")
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public Mono<Void> deleteById(@PathVariable UUID eventID) {
+        return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN));
     }
 }
