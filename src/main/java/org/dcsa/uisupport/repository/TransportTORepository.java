96e320822560faad7d749dc2ac70da2702e4a84d
Niels Thykier <nt@asseco.dk>
2021-09-02 14:41:39 +0200

    Remove obsolete Transport code
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/uisupport/repository/TransportTORepository.java
+++
@@ -1,8 +0,0 @@
-package org.dcsa.uisupport.repository;
-
-import org.dcsa.core.repository.ExtendedRepository;
-import org.dcsa.uisupport.model.transferobjects.TransportTO;
-
-import java.util.UUID;
-
-public interface TransportTORepository extends ExtendedRepository<TransportTO, UUID> {}
