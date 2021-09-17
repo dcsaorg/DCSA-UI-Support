c619f776b53a0be8724094f61409e22bae0e85dc
Niels Thykier <nt@asseco.dk>
2021-09-17 11:04:44 +0200

    Add endpoint for ports and terminals
    Signed-off-by: Niels Thykier <nt@asseco.dk>

---
+++ src/main/java/org/dcsa/uisupport/repository/PortWithTimezoneRepository.java
@@ -0,0 +1,7 @@
+package org.dcsa.uisupport.repository;
+
+import org.dcsa.core.repository.ExtendedRepository;
+import org.dcsa.uisupport.model.PortWithTimezone;
+
+public interface PortWithTimezoneRepository extends ExtendedRepository<PortWithTimezone, String> {
+}
