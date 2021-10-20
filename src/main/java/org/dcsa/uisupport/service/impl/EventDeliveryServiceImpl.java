ac565a3ba72f1bc2aad6d843c1dadb9b06ae617b
Niels Thykier <nt@asseco.dk>
2021-10-20 21:09:56 +0200

    Support fetching delivery status by TC ID
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/uisupport/service/impl/EventDeliveryServiceImpl.java
+++ src/main/java/org/dcsa/uisupport/service/impl/EventDeliveryServiceImpl.java
@@ -1,10 +1,9 @@
 package org.dcsa.uisupport.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.dcsa.core.events.model.Event;
 import org.dcsa.core.events.repository.EventRepository;
+import org.dcsa.core.exception.NotFoundException;
 import org.dcsa.core.service.impl.ExtendedBaseServiceImpl;
-import org.dcsa.uisupport.model.AttemptedEventDelivery;
 import org.dcsa.uisupport.model.EventDelivery;
 import org.dcsa.uisupport.model.PendingEvent;
 import org.dcsa.uisupport.model.enums.EventDeliveryStatus;
@@ -23,54 +22,10 @@ import java.util.UUID;
 public class EventDeliveryServiceImpl extends ExtendedBaseServiceImpl<EventDeliveryRepository, EventDelivery, UUID>
         implements EventDeliveryService {
     private final EventDeliveryRepository eventDeliveryRepository;
-    private final EventRepository eventRepository;
 
     @Override
     public EventDeliveryRepository getRepository() {
         return eventDeliveryRepository;
     }
 
-    @Override
-    public Mono<EventDelivery> findById(UUID eventID) {
-        Mono<EventDelivery> unmappedEventDelivery = eventDeliveryRepository
-                .findUnmappedEventByEventId(eventID)
-                .map(event -> pendingDelivery());
-        Mono<EventDelivery> pendingEventDelivery = eventDeliveryRepository
-                .findPendingEventByEventId(eventID)
-                .map(event -> {
-                    if (event.getLastErrorMessage() == null) {
-                        return pendingDelivery();
-                    }
-                    return attemptedEventDelivery(event);
-                });
-        Mono<EventDelivery> mappedEventDelivery = Flux.merge(pendingEventDelivery, unmappedEventDelivery)
-                .switchIfEmpty(finishedDelivery())
-                .next();
-
-        return eventRepository.findById(eventID)
-                .flatMap(e -> mappedEventDelivery)
-                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)));
-    }
-
-    private AttemptedEventDelivery attemptedEventDelivery(PendingEvent pendingEvent) {
-        AttemptedEventDelivery eventDelivery = new AttemptedEventDelivery();
-        eventDelivery.setEventDeliveryStatus(EventDeliveryStatus.ATTEMPTED_DELIVERY);
-        eventDelivery.setEqueuedAtDateTime(pendingEvent.getEqueuedAtDateTime());
-        eventDelivery.setLastAttemptDateTime(pendingEvent.getLastAttemptDateTime());
-        eventDelivery.setLastErrorMessage(pendingEvent.getLastErrorMessage());
-        eventDelivery.setRetryCount(pendingEvent.getRetryCount());
-        return eventDelivery;
-    }
-
-    private EventDelivery pendingDelivery() {
-        EventDelivery eventDelivery = new EventDelivery();
-        eventDelivery.setEventDeliveryStatus(EventDeliveryStatus.PENDING_DELIVERY);
-        return eventDelivery;
-    }
-
-    private Flux<EventDelivery> finishedDelivery() {
-        EventDelivery eventDelivery = new EventDelivery();
-        eventDelivery.setEventDeliveryStatus(EventDeliveryStatus.DELIVERY_FINISHED);
-        return Flux.just(eventDelivery);
-    }
 }
