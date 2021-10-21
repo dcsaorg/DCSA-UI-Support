cef78a2d39b26322b99223f6c057d330ecf63d0e
abf <abf@asseco.dk>
2021-10-21 11:26:48 +0200

    Timestamp-definition-controller-skeletion
    - repository, service and controller skeleton created for timestamp-definition.

---
+++ src/main/java/org/dcsa/uisupport/repository/TimestampDefinitionRepository.java
@@ -0,0 +1,7 @@
+package org.dcsa.uisupport.repository;
+
+import org.dcsa.core.events.model.TimestampDefinition;
+import org.dcsa.core.repository.ExtendedRepository;
+
+public interface TimestampDefinitionRepository extends ExtendedRepository<TimestampDefinition,String> {
+}
