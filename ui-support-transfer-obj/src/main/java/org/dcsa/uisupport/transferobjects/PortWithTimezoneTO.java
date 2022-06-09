8cbe80c117427e76980f5791c81cdb366c617b3f
Finn Nielsen <fn@asseco.dk>
2022-06-09 13:01:48 +0200

    fix port timezone to be more like the legacy version

--- ui-support-transfer-obj/src/main/java/org/dcsa/uisupport/transferobjects/PortWithTimezoneTO.java
+++ ui-support-transfer-obj/src/main/java/org/dcsa/uisupport/transferobjects/PortWithTimezoneTO.java
@@ -12,7 +12,13 @@ public record PortWithTimezoneTO(
   @Size(max = 100) @JsonProperty("unLocationName")
   String unLocationName,
 
-  @JsonProperty("ianaTimezone")
+  @Size(max = 3) @JsonProperty("locationCode")
+  String locationCode,
+
+  @Size(max = 2) @JsonProperty("countryCode")
+  String countryCode,
+
+  @JsonProperty("timezone")
   String ianaTimezone
 ) {
   @Builder // workaround for intellij issue
