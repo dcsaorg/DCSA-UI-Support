47f85bddbe4be26b4098f5190b2d339410a19cb2
Martin Petersson <mp@asseco.dk>
2022-06-16 10:55:40 +0200

    Update JacksonConfiguration.java

--- ui-support-service/src/main/java/org/dcsa/uisupport/configuration/JacksonConfiguration.java
+++ ui-support-service/src/main/java/org/dcsa/uisupport/configuration/JacksonConfiguration.java
@@ -6,13 +6,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 import java.text.SimpleDateFormat;
 
 @Configuration
 public class JacksonConfiguration {
-  @Bean()
+  @Bean
   public ObjectMapper defaultObjectMapper() {
     return new ObjectMapper()
       .enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
