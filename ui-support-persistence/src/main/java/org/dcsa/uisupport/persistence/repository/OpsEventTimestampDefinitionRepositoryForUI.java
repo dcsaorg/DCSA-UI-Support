f54a06b8f105b51706479684008b3784619bff00
Martin Petersson <mp@asseco.dk>
2022-06-16 16:08:12 +0200

    Remove OpsEventTimestampDefinition

--- ui-support-persistence/src/main/java/org/dcsa/uisupport/persistence/repository/OpsEventTimestampDefinitionRepositoryForUI.java
+++ ui-support-persistence/src/main/java/org/dcsa/uisupport/persistence/repository/OpsEventTimestampDefinitionRepositoryForUI.java
@@ -1,6 +1,6 @@
 package org.dcsa.uisupport.persistence.repository;
 
-import org.dcsa.uisupport.persistence.entity.OpsEventTimestampDefinition;
+import org.dcsa.jit.persistence.entity.OpsEventTimestampDefinition;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
