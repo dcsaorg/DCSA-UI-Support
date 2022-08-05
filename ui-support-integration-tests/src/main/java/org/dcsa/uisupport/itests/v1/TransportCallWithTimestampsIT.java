579d6596774712b139357fbbfcf081e7b23e5107
abf <abf@asseco.dk>
2022-08-05 16:47:11 +0200

    DDT-1154: add IT tests for TC QueryParameters

--- ui-support-integration-tests/src/main/java/org/dcsa/uisupport/itests/v1/TransportCallWithTimestampsIT.java
+++ ui-support-integration-tests/src/main/java/org/dcsa/uisupport/itests/v1/TransportCallWithTimestampsIT.java
@@ -1,6 +1,7 @@
 package org.dcsa.uisupport.itests.v1;
 
 import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
 import org.dcsa.jit.transferobjects.TransportCallTO;
 import org.dcsa.jit.transferobjects.VesselTO;
 import org.dcsa.jit.transferobjects.enums.FacilityCodeListProvider;
@@ -13,7 +14,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TransportCallWithTimestampsIT {
@@ -24,59 +25,114 @@ public class TransportCallWithTimestampsIT {
 
   @Test
   public void testGetTransportCalls() {
-    List<TransportCallWithTimestampsTO> calls = given()
-      .contentType("application/json")
+    given()
+        .contentType("application/json")
+        .get("/v1/unofficial/transport-calls")
+        .then()
+        .assertThat()
+        .statusCode(200)
+        .contentType(ContentType.JSON)
+        .body("size()", greaterThanOrEqualTo(3))
+        .extract()
+        .body()
+        .jsonPath()
+        .getList(".", TransportCallWithTimestampsTO.class);
+  }
+  @Test
+  public void testGetTransportCallsWithVesselIMONumberQueryParameter() {
+    given()
+      .contentType(ContentType.JSON)
+      .queryParam("vessel.vesselIMONumber", "1234567")
       .get("/v1/unofficial/transport-calls")
       .then()
       .assertThat()
-      .statusCode(200)
+      .statusCode(HttpStatus.SC_OK)
       .contentType(ContentType.JSON)
-      .body("size()", greaterThanOrEqualTo(3))
+      .body("size()", greaterThanOrEqualTo(1))
+      .body("transportCall.vessel.vesselIMONumber", everyItem(equalTo("1234567")))
       .extract()
       .body()
-      .jsonPath().getList(".", TransportCallWithTimestampsTO.class)
-      ;
+      .jsonPath()
+      .getList(".", TransportCallWithTimestampsTO.class);
   }
 
   @Test
-  public void testPostTransportCalls() {
-    TransportCallTO newTransportCall = TransportCallTO.builder()
-      .exportVoyageNumber("2107E")
-      .importVoyageNumber("2106W")
-      .carrierServiceCode("TNT1")
-      .modeOfTransport(ModeOfTransport.VESSEL)
-      .UNLocationCode("AEJEA")
-      .facilityCode("DPWJA")
-      .facilityCodeListProvider(FacilityCodeListProvider.SMDG)
-      .vessel(VesselTO.builder().vesselIMONumber("1234567").build())
-      .build();
-
-    TransportCallWithTimestampsTO saved = given()
-      .contentType("application/json")
-      .body(newTransportCall)
-      .post("/v1/unofficial/transport-calls")
-      .then()
-      .assertThat()
-      .statusCode(201)
+  public void testGetTransportCallsWithUNLocationCodeQueryParameter() {
+    given()
       .contentType(ContentType.JSON)
-      .extract()
-      .body()
-      .jsonPath().getObject(".", TransportCallWithTimestampsTO.class)
-      ;
-
-    List<TransportCallWithTimestampsTO> calls = given()
-      .contentType("application/json")
+      .queryParam("facility.UNLocationCode", "USMIA")
       .get("/v1/unofficial/transport-calls")
       .then()
       .assertThat()
-      .statusCode(200)
+      .statusCode(HttpStatus.SC_OK)
       .contentType(ContentType.JSON)
-      .body("size()", greaterThanOrEqualTo(3))
+      .body("size()", greaterThanOrEqualTo(1))
+      .body("transportCall.UNLocationCode", everyItem(equalTo("USMIA")))
       .extract()
       .body()
-      .jsonPath().getList(".", TransportCallWithTimestampsTO.class)
-      ;
+      .jsonPath()
+      .getList(".", TransportCallWithTimestampsTO.class);
+  }
+  @Test
+  public void testGetTrandsportCallsWithUNLocationCodeQueryParameter() {
+    given()
+      .contentType(ContentType.JSON)
+      .queryParam("facility.facilitySMDGCode", "USMIA")
+      .get("/v1/unofficial/transport-calls")
+      .then()
+      .assertThat()
+      .statusCode(HttpStatus.SC_OK)
+      .contentType(ContentType.JSON)
+      .body("size()", greaterThanOrEqualTo(1))
+      .body("transportCall.UNLocationCode", everyItem(equalTo("USMIA")))
+      .extract()
+      .body()
+      .jsonPath()
+      .getList(".", TransportCallWithTimestampsTO.class);
+  }
+  @Test
+  public void testPostTransportCalls() {
+    TransportCallTO newTransportCall =
+        TransportCallTO.builder()
+            .exportVoyageNumber("2107E")
+            .importVoyageNumber("2106W")
+            .carrierServiceCode("TNT1")
+            .modeOfTransport(ModeOfTransport.VESSEL)
+            .UNLocationCode("AEJEA")
+            .facilityCode("DPWJA")
+            .facilityCodeListProvider(FacilityCodeListProvider.SMDG)
+            .vessel(VesselTO.builder().vesselIMONumber("1234567").build())
+            .build();
 
-    assertTrue(calls.stream().anyMatch(tc -> tc.getTransportCallID().equals(saved.getTransportCallID())));
+    TransportCallWithTimestampsTO saved =
+        given()
+            .contentType("application/json")
+            .body(newTransportCall)
+            .post("/v1/unofficial/transport-calls")
+            .then()
+            .assertThat()
+            .statusCode(201)
+            .contentType(ContentType.JSON)
+            .extract()
+            .body()
+            .jsonPath()
+            .getObject(".", TransportCallWithTimestampsTO.class);
+
+    List<TransportCallWithTimestampsTO> calls =
+        given()
+            .contentType("application/json")
+            .get("/v1/unofficial/transport-calls")
+            .then()
+            .assertThat()
+            .statusCode(200)
+            .contentType(ContentType.JSON)
+            .body("size()", greaterThanOrEqualTo(3))
+            .extract()
+            .body()
+            .jsonPath()
+            .getList(".", TransportCallWithTimestampsTO.class);
+
+    assertTrue(
+        calls.stream().anyMatch(tc -> tc.getTransportCallID().equals(saved.getTransportCallID())));
   }
 }
